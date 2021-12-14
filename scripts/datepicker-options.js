d2bc5ff9f394b03f0a343e6d8ccc911e0098d796
Yanara <yanaracaballero97@gamil.com>
2021-12-14 17:12:25 -0500

    Initial Commit

---
+++ scripts/datepicker-options.js
@@ -0,0 +1,11 @@
+﻿// Initialize Bootstrap DatePicker -> only html input
+$(function () {
+    $(".datepicker").datepicker({
+        todayBtn: "linked",
+        weekStart: 1,
+        todayHighlight: true,
+        autoclose: true,
+        orientation: "bottom",
+        disableTouchKeyboard: true
+    });
+});
