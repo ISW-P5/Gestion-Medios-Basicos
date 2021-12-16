625f023d147cdb849b5be5ebf0e5d863b0c0dc40
MacKey-255 <mackeyfuturo@gmail.com>
2021-12-15 23:53:00 -0500

    Fix error when validate fields Added more validation methods in validator.js

--- scripts/validator.js
+++ scripts/validator.js
@@ -3,12 +3,18 @@ const validator = {
     // Validator
     isEmpty: (value) => value.length === 0 || !value.trim(),
     isNull: (value) => value === undefined || value === null,
+    isNumber: (value) => !isNaN(value),
+    isDate: (value) => !isNaN(new Date(value).getDate()),
     minLength: (n, value) => value.length < n,
+    equalLength: (n, value) => value.length === n,
     maxLength: (n, value) => value.length > n,
     regex: (regex, value) => regex.test(value),
     email: (email) => email.match(
         /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     ),
+    onlyNumbers(value) { return this.regex(/^\d+$/, value); },
+    onlyLetters(value) { return this.regex(/^[a-zA-Z]+$/, value); },
+    onlyLettersAndNumbers(value) { return this.regex(/^[a-zA-Z0-9]+$/i, value); },
 
     // Extractors
     getValueInput: (input_name) => $('input[name="' + input_name + '"]').val(),
